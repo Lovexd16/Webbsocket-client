a8a46ca4f0a59415e42e96f8acfd23087b4af74b
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-26 13:01:11 +0200

    chat färdig

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("Hej");
 
 const socket = new SockJS("http://localhost:8080/websocket");
 const messageList = document.getElementById("messageList");
+const inputText = document.getElementById("inputText");
+const sendBtn = document.getElementById("sendBtn");
 
 const stompClient = Stomp.over(socket);
 
@@ -16,9 +18,25 @@ stompClient.connect({}, (frame) => {
     messageList.appendChild(li);
   });
 
+  stompClient.subscribe("/topic/chat", (chat) => {
+    console.log("chat", JSON.parse(chat.body).chat);
+
+    let li = document.createElement("li");
+    li.innerText = JSON.parse(chat.body).chat;
+    messageList.appendChild(li);
+  });
+
   sendHello("Love");
 });
 //Egentligen använder man name från en inloggningsprocedur
 function sendHello(name) {
   stompClient.send("/app/hello", {}, JSON.stringify({ name: name }));
 }
+
+sendBtn.addEventListener("click", () => {
+  stompClient.send(
+    "/app/chat",
+    {},
+    JSON.stringify({ content: sendText.value })
+  );
+});
